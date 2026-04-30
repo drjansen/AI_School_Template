b41d75b0a09684b1c5db2addabbd8634bf549be1
Dr. Jansen <dr.jansen@ivycollegiateschool.org>
2026-04-30 22:30:52 +0900

    naming error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
                         }}
                     >
                         <Image
-                            src="/brand/eladrin-logo.png"
+                            src="/brand/eldarin-logo.png"
                             alt={`${brand.name} logo`}
                             width={1350}
                             height={600}
